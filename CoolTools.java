78d0f83ac209ab4354cc94acae8493117ce58385
auhsojallidap <auhsojallidap@gmail.com>
2026-09-12 07:11:49 +0800

    update CoolTools.java

--- CoolTools.java
+++ CoolTools.java
@@ -10,7 +10,7 @@ public class CoolTools {
     asciiArt();
     while (!loop){
       System.out.println("");
-      System.out.println("===SECURITY===");
+      System.out.println("===SECURITyyyyY===");
        System.out.print("PIN : ");
       String num1 = imp.next();
 
